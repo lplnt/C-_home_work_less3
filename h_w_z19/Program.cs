b97e31680ab48b25fa8e2ac3f1d36c7b6fe5a8e5
lplnt <phd@live.ru>
2022-09-28 21:47:38 +0300

    C# home work lessen 3

---
+++ h_w_z19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear(); //очистка консоли
+Console.WriteLine("Введите цифру которую хотите проверить: "); //вывод сообщение 
+int num = Convert.ToInt32(Console.ReadLine()); //ввод числа
+int rev = 0;
+int temp = num;
+while(num > 0)
+{
+    int dig = num % 10;
+    rev = rev * 10 + dig;
+    num = num / 10;
+}
+if (temp == rev)
+{
+    Console.WriteLine("Это палиндром!");
+}
+else
+{
+    Console.WriteLine("Это не палиндром!");
+}
---
+++ h_w_z21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// AB = √(xb - xa)^2 + (yb - ya)^2 + (zb - za)^2
+
+Console.Clear(); //очистка консоли
+Console.WriteLine("Введите координаты первой точки: "); //вывод сообщение 
+double[] p1 = new double[3] {Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine())}; //ввод трех чисел первой точки
+Console.WriteLine("Введите координаты первой точки: "); //вывод сообщение 
+double[] p2 = new double[3] {Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine())}; //ввод трех чисел второй точки
+
+double distance = Math.Pow((p2[0]-p1[0]),2) + Math.Pow((p2[1]-p1[1]),2) + Math.Pow((p2[2]-p1[2]),2);
+
+Console.WriteLine(distance);
+
+
+
+
+
+
+
+
+
